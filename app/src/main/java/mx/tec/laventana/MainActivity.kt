74641e21022a0f806443021a582910cea5476c9e
ArmandoAV <armando.arredondo.valle@gmail.com>
2023-08-29 13:19:29 -0600

    Adds binding.

--- app/src/main/java/mx/tec/laventana/MainActivity.kt
+++ app/src/main/java/mx/tec/laventana/MainActivity.kt
@@ -2,10 +2,13 @@ package mx.tec.laventana
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import mx.tec.laventana.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
